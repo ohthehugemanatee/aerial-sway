f2511aac072564a05152d735d0a4f5456c6ae984
Campbell Vertesi <campbell@vertesi.com>
2022-03-29 18:26:53 +0200

    Update for new path

--- aerial-wayland.sh
+++ aerial-wayland.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+if pgrep -x "swaylock" > /dev/null; then
+  exit 0
+fi
 
 COUNT_OUTPUTS=$(swaymsg -t get_outputs | jq '. | length' | tr -d '\n')
 
 if [ $COUNT_OUTPUTS -gt 1 ]; then
-  swaymsg output eDP-1 disable
+  EDP1_DISABLED=true
+  /usr/bin/swaymsg output eDP-1 disable
 fi
-cvlc -Z /home/ohthehugemanatee/tmp/screensaver/playlist.m3u --fullscreen --no-mouse-events --no-keyboard-events  --loop --no-osd --no-audio --video-wallpaper & swaylock -c 00000000 && kill $!
 
+/usr/bin/cvlc -Z /home/ohthehugemanatee/Projects/personal/screensaver/playlist.m3u --fullscreen --no-mouse-events --no-keyboard-events  --loop --no-osd --no-audio --video-wallpaper & /usr/bin/swaylock -c 00000000 && kill $!
 
-if [ $COUNT_OUTPUTS -gt 1 ]; then
-  swaymsg output eDP-1 enable
-fi
+$EDP1_DISABLED && /usr/bin/swaymsg output eDP-1 enable
