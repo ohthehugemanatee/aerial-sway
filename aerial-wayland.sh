10c7e74da41780b0a37dbc259dee8067163c1185
Campbell Vertesi <campbell@vertesi.com>
2022-05-03 19:24:53 +0200

    Disable laptop monitor via dpms
    see https://github.com/swaywm/sway/issues/6962

--- aerial-wayland.sh
+++ aerial-wayland.sh
@@ -8,9 +8,9 @@ COUNT_OUTPUTS=$(swaymsg -t get_outputs | jq '. | length' | tr -d '\n')
 
 if [ $COUNT_OUTPUTS -gt 1 ]; then
   EDP1_DISABLED=true
-  /usr/bin/swaymsg output eDP-1 disable
+  /usr/bin/swaymsg output eDP-1 dpms off
 fi
 
 /usr/bin/cvlc -Z /home/ohthehugemanatee/Projects/personal/screensaver/playlist.m3u --fullscreen --no-mouse-events --no-keyboard-events  --loop --no-osd --no-audio --video-wallpaper & /usr/bin/swaylock -c 00000000 && kill $!
 
-$EDP1_DISABLED && /usr/bin/swaymsg output eDP-1 enable
+$EDP1_DISABLED && /usr/bin/swaymsg output eDP-1 dpms on
