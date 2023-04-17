7b9d25ea075b1f85471e7e29f7d443f13e25516c
Campbell Vertesi <campbell@vertesi.com>
2023-04-17 13:44:13 +0200

    Handle multi-head and different lockers

--- aerial-wayland.sh
+++ aerial-wayland.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
-if pgrep -x "swaylock" > /dev/null; then
+# Starts the screensaver using the playlist and videos created by setup.sh . There are no arguments.
+
+# ********
+# Before first run, please check these variables against your own system: 
+# ********
+
+# If you have a multi-monitor setup (even sometimes), which outputs to disable during the screensaver. Get the names of all your outputs from `swaymsg -t get_outputs` . Separate each output with a space.
+DISABLE_OUTPUTS=eDP1
+
+# Program to use for locking the screen.
+LOCKER=/usr/bin/swaylock
+
+# ********
+# No other modifications should be necessary.
+# ********
+
+SCRIPTDIR=$(dirname -- "$( readlink -f -- \"$0\"; )")
+
+# Exit if the screen is already locked.
+LOCKERBIN=$(basename "${LOCKER}")
+if pgrep -x "${LOCKERBIN}" > /dev/null; then
   exit 0
 fi
 
-COUNT_OUTPUTS=$(swaymsg -t get_outputs | jq '. | length' | tr -d '\n')
+# If multiple outputs, disable according to ${DISABLE_OUTPUTS}.
+toggle_outputs() {
+  for OUTPUT_NAME in ${DISABLE_OUTPUTS}; do
+    /usr/bin/swaymsg output ${OUTPUT_NAME} dpms $1
+  done 
+}
 
+COUNT_OUTPUTS=$(swaymsg -t get_outputs | jq '. | length' | tr -d '\n')
 if [ $COUNT_OUTPUTS -gt 1 ]; then
-  EDP1_DISABLED=true
-  /usr/bin/swaymsg output eDP-1 dpms off
+  OUTPUTS_DISABLED=true
+  toggle_outputs off
 fi
 
-/usr/bin/cvlc -Z /home/ohthehugemanatee/Projects/personal/screensaver/playlist.m3u --fullscreen --no-mouse-events --no-keyboard-events  --loop --no-osd --no-audio --video-wallpaper & /usr/bin/swaylock -c 00000000 && kill -9 $!
+/usr/bin/cvlc -Z "${SCRIPTDIR}/videos/playlist.m3u" --fullscreen --no-mouse-events --no-keyboard-events  --loop --no-osd --no-audio --video-wallpaper & /usr/bin/swaylock -c 00000000 && kill -9 $!
 
-$EDP1_DISABLED && /usr/bin/swaymsg output eDP-1 dpms on
+$OUTPUTS_DISABLED && toggle_outputs on
